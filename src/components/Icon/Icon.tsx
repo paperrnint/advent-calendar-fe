781e8f6c9a79fcb31bc9f34e756fde76e89f2dfc
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2025-10-28 12:58:13 +0900

    fix: update icon file extension to webp (#5)

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const Icon = ({ number, size = 80, className = '', alt }: Props) => {
   return (
     <Image
-      src={`/images/${number}.png`}
+      src={`/images/${number}.webp`}
       alt={alt || `크리스마스 아이콘 ${number}`}
       width={size}
       height={size}
