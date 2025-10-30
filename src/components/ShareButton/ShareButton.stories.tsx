3405f663813e4039a3e449a1bfee7b2bd78f3245
Eunji <eeunjiilee@gmail.com>
2025-10-30 13:15:38 +0900

    feat: add ShareButton component

---
+++ src/components/ShareButton/ShareButton.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ShareButton } from './ShareButton';
+
+const meta = {
+  title: 'Buttons/ShareButton',
+  component: ShareButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ShareButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
