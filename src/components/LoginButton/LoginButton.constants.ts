7d5ca2d4ead6abd9ba3dbf8227d5f0db1f45bddb
Eunji <eeunjiilee@gmail.com>
2025-10-30 13:13:50 +0900

    feat: add LoginButton component

---
+++ src/components/LoginButton/LoginButton.constants.ts
@@ -0,0 +1,12 @@
+export const loginConfig = {
+  naver: {
+    icon: '/svg/naver-logo.svg',
+    text: '네이버 로그인',
+    style: 'bg-naver text-white',
+  },
+  kakao: {
+    icon: '/svg/kakao-logo.svg',
+    text: '카카오 로그인',
+    style: 'bg-kakao text-black/85',
+  },
+} as const;
