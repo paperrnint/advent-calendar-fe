a60817f0ccd45e076578f8b3dc8381e80977dd1a
Eunji <eeunjiilee@gmail.com>
2025-11-13 10:46:00 +0900

    feat: add auth store

---
+++ src/stores/authStore.ts
@@ -0,0 +1,22 @@
+import { RibbonColor } from '@/constants';
+import { atom } from 'jotai';
+
+export interface UserState {
+  uuid: string | null;
+  name: string | null;
+  color: RibbonColor | null;
+  isAuthenticated: boolean;
+}
+
+const initialState: UserState = {
+  uuid: null,
+  name: null,
+  color: null,
+  isAuthenticated: false,
+};
+
+export const userAtom = atom<UserState>(initialState);
+
+export const authLoadingAtom = atom<boolean>(false);
+
+export const authErrorAtom = atom<string | null>(null);
