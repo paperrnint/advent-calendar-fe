fe1a46151f4d556ff5615c0e5fa46070ab3e3be2
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2025-11-13 14:49:42 +0900

    feat: implement server side user data fetching for advent calendar page (#25)
    * feat: add fetchApi utility function
    
    * feat: implement server side user data fetching for advent calendar page

--- src/components/LinkButton/LinkButton.stories.tsx
+++ src/components/LinkButton/LinkButton.stories.tsx
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { NewButton } from './NewButton';
+import { LinkButton } from './LinkButton';
 
 const meta = {
-  title: 'Buttons/NewButton',
-  component: NewButton,
+  title: 'Buttons/LinkButton',
+  component: LinkButton,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-} satisfies Meta<typeof NewButton>;
+} satisfies Meta<typeof LinkButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -16,5 +16,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     children: '내 어드벤트 캘린더 만들기',
+    href: '/',
   },
 };
