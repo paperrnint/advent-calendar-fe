2637d0af877dd27eba4a01e7ae18ed3de5c86340
Eunji <eeunjiilee@gmail.com>
2025-11-15 22:14:14 +0900

    feat: add storybook stories for navigation components

---
+++ src/components/NavigationBar/NavigationBar.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { NavigationBar } from './NavigationBar';
+
+const meta = {
+  title: 'Components/NavigationBar',
+  component: NavigationBar,
+  parameters: {
+    layout: 'padded',
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: '/',
+        query: {},
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className="p-20">
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof NavigationBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const AuthenticatedMode: Story = {
+  args: {
+    isAuthenticated: true,
+  },
+  parameters: {
+    nextjs: {
+      navigation: {
+        push: () => console.log('Navigate to calendar'),
+      },
+    },
+  },
+};
+
+export const UnauthenticatedMode: Story = {
+  args: {
+    isAuthenticated: false,
+  },
+  parameters: {
+    nextjs: {
+      navigation: {
+        push: () => console.log('Navigate to home'),
+      },
+    },
+  },
+};
