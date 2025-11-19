c6fbc1baad90a7dd3eaf576a2669ac81d1e8326c
Eunji <eeunjiilee@gmail.com>
2025-11-19 10:51:51 +0900

    chore: vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,14 @@ export default defineConfig({
   test: {
     projects: [
       {
-        extends: true,
+        test: {
+          name: 'unit',
+          environment: 'node',
+          include: ['src/**/*.{test,spec}.{ts,tsx}'],
+          exclude: ['src/**/*.stories.{ts,tsx}', 'node_modules/**'],
+        },
+      },
+      {
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
